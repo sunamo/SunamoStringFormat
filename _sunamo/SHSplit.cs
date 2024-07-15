d495af7b79fd81833c6f93f7ce6a812123d25f97
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-15 21:38:30 +0200

    Before renaming all *SH and *SE

---
+++ _sunamo/SHSplit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunamoStringFormat._sunamo;
+internal class SHSplit
+{
+    internal static List<string> SplitMore(string item, params string[] space)
+    {
+        return item.Split(space, StringSplitOptions.RemoveEmptyEntries).ToList();
+    }
+
+    internal static List<string> SplitCharMore(string v1, params char[] v2)
+    {
+        return v1.Split(v2).ToList();
+    }
+}
--- _sunamo/SunamoStringReplace/SHReplace.cs
+++ _sunamo/SunamoStringReplace/SHReplace.cs
@@ -5,8 +5,8 @@ internal class SHReplace
     {
         if (pairLines)
         {
-            var from2 = SHSunamoExceptions.Split(co, Environment.NewLine);
-            var to2 = SHSunamoExceptions.Split(zaCo, Environment.NewLine);
+            var from2 = SHSplit.SplitMore(co, Environment.NewLine);
+            var to2 = SHSplit.SplitMore(zaCo, Environment.NewLine);
             ThrowEx.DifferentCountInLists("from2", from2, "to2", to2);
 
             for (int i = 0; i < from2.Count; i++)
