b11d3cf9b9359d1b5953024af4ede1bfa6a1162a
Radek Jancik <radek.jancik@usys.cz>
2025-10-21 20:42:56 +0200

    feat: Replace variables names with self-describe names

--- SunamoStringFormat/_sunamo/CA.cs
+++ SunamoStringFormat/_sunamo/CA.cs
@@ -1,3 +1,6 @@
+// EN: Variable names have been checked and replaced with self-descriptive names
+// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
+
 namespace SunamoStringFormat._sunamo;
 
 internal class CA
@@ -32,20 +35,20 @@ internal class CA
 
     internal static bool IsListStringWrappedInArray(IEnumerable v2)
     {
-        var c = 0;
+        var count = 0;
         object first = null;
         foreach (var item in v2)
         {
-            if (c == 0)
+            if (count == 0)
             {
                 first = item;
             }
 
-            c++;
+            count++;
         }
 
 
-        if (c == 1 && (first == "System.Collections.Generic.List`1[System.String]" ||
+        if (count == 1 && (first == "System.Collections.Generic.List`1[System.String]" ||
                        first == "System.Collections.Generic.List`1[System.Object]")) return true;
         return false;
     }
